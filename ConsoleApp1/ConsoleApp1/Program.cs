47c33557ce92493deaea018a0b6c797518b9792e
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-04 12:20:42 +0400

    Task Complete

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "";
+                Console.WriteLine("Bir ay daxil edin:");
+                x = Console.ReadLine();
+                switch (x)
+                {
+                    case "yanvar":
+                    Console.WriteLine(x + " ayinda 31 gun Var");
+                    break;
+                    case "mart":
+                    Console.WriteLine(x + " ayinda 31 gun Var");
+                    break;
+                    case "may":
+                    Console.WriteLine(x + " ayinda 31 gun Var");
+                    break;
+                    case "iyul":
+                    Console.WriteLine(x + " ayinda 31 gun Var");
+                    break;
+                    case "avgust":
+                    Console.WriteLine(x + " yinda 31 gun Var");
+                    break;
+                    case "oktiyabr":
+                    Console.WriteLine(x + " ayinda 31 gun Var");
+                    break;
+                    case "dekabr":
+                    Console.WriteLine(x + " ayinda 31 gun Var");
+                    break;
+
+                    case "aprel":
+                    Console.WriteLine(x + " ayinda 30 gun Var");
+                    break;
+                    case "iyun":
+                    Console.WriteLine(x + " ayinda 30 gun Var");
+                    break;
+                    case "sentiyabr":
+                    Console.WriteLine(x + " ayinda 30 gun Var");
+                    break;
+                    case "noyabr":
+                    Console.WriteLine(x + " ayinda 30 gun Var");
+                    break;
+
+                    case "fevral":
+                    Console.WriteLine(x + " ayinda 28 veya 29 gun Var");
+                    break;
+                    default:
+                        Console.WriteLine("Ay duzgun daxil edilmeyib");
+                        break;
+                }
+        }
+    }
+}
